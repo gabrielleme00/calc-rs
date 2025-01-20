a2ff68e257b78f920bcaa2d53a9ff713339103b3
Gabriel Leme <gabriel.leme@luizalabs.com>
2025-01-20 16:21:17 -0300

    feat: bitwise operations

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,24 +1,29 @@
+/// Unary operation.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum UnOperator {
+enum UnOperation {
     Plus,
     Minus,
 }
 
+/// Binary operation.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum BiOperator {
+enum BiOperation {
     Add,
     Sub,
     Mul,
     Div,
     Mod,
     Pow,
+    And,
+    Or,
+    Xor,
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Token {
     Operand(i32),
-    BiOp(BiOperator),
-    UnOp(UnOperator),
+    BiOperator(BiOperation),
+    UnOperator(UnOperation),
     Bracket(char),
 }
 
@@ -33,10 +38,10 @@ pub enum CalcError {
 impl std::fmt::Display for CalcError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CalcError::BadToken(c) => write!(f, "Bad token encountered: '{}'", c),
-            CalcError::MismatchedParens => write!(f, "Mismatched parentheses"),
-            CalcError::MissingOperand => write!(f, "Missing operand"),
-            CalcError::UnusedTokens => write!(f, "There are unused tokens"),
+            Self::BadToken(c) => write!(f, "Bad token encountered: '{}'", c),
+            Self::MismatchedParens => write!(f, "Mismatched parentheses"),
+            Self::MissingOperand => write!(f, "Missing operand"),
+            Self::UnusedTokens => write!(f, "There are unused tokens"),
         }
     }
 }
@@ -48,53 +53,68 @@ fn as_digit(c: char) -> i32 {
 
 /// Tokenizes a string of an infix mathematical expression.
 fn parse<T: AsRef<str>>(expr: T) -> Result<Vec<Token>, CalcError> {
+    use Token::*;
+    use BiOperation::*;
+    use UnOperation::*;
+    use CalcError::*;
+
     let expr = expr.as_ref();
     let chars = expr.chars();
     let mut tokens: Vec<Token> = Vec::new();
     let mut parens: Vec<char> = Vec::new();
+
     for c in chars {
         match c {
             '0'..='9' => match tokens.last_mut() {
-                Some(Token::Operand(n)) => *n = *n * 10 + as_digit(c),
-                _ => tokens.push(Token::Operand(as_digit(c))),
+                Some(Operand(n)) => *n = *n * 10 + as_digit(c),
+                _ => tokens.push(Operand(as_digit(c))),
             },
             '(' => {
-                tokens.push(Token::Bracket(c));
+                tokens.push(Bracket(c));
                 parens.push(c);
             }
             ')' => {
-                tokens.push(Token::Bracket(c));
+                tokens.push(Bracket(c));
                 if let Some(p) = parens.pop() {
                     if p != '(' {
-                        return Err(CalcError::MismatchedParens);
+                        return Err(MismatchedParens);
                     }
                 } else {
-                    return Err(CalcError::MismatchedParens);
+                    return Err(MismatchedParens);
                 }
             }
             '+' => match tokens.last() {
-                Some(Token::Operand(_)) | Some(Token::Bracket(')')) => {
-                    tokens.push(Token::BiOp(BiOperator::Add))
+                Some(Operand(_)) | Some(Bracket(')')) => {
+                    tokens.push(BiOperator(Add))
                 }
-                _ => tokens.push(Token::UnOp(UnOperator::Plus)),
+                _ => tokens.push(UnOperator(Plus)),
             },
             '-' => match tokens.last() {
-                Some(Token::Operand(_)) | Some(Token::Bracket(')')) => {
-                    tokens.push(Token::BiOp(BiOperator::Sub))
+                Some(Operand(_)) | Some(Bracket(')')) => {
+                    tokens.push(BiOperator(Sub))
                 }
-                _ => tokens.push(Token::UnOp(UnOperator::Minus)),
+                _ => tokens.push(UnOperator(Minus)),
             },
-            '*' => tokens.push(Token::BiOp(BiOperator::Mul)),
-            '/' => tokens.push(Token::BiOp(BiOperator::Div)),
-            '%' => tokens.push(Token::BiOp(BiOperator::Mod)),
-            '^' => tokens.push(Token::BiOp(BiOperator::Pow)),
+            '*' => {
+                if tokens.ends_with(&[BiOperator(Mul)]) {
+                    tokens.pop();
+                    tokens.push(BiOperator(Pow));
+                } else {
+                    tokens.push(BiOperator(Mul))
+                }
+            },
+            '/' => tokens.push(BiOperator(Div)),
+            '%' => tokens.push(BiOperator(Mod)),
+            '&' => tokens.push(BiOperator(And)),
+            '|' => tokens.push(BiOperator(Or)),
+            '^' => tokens.push(BiOperator(Xor)),
             ' ' | '\n' | '\r' => {}
-            _ => return Err(CalcError::BadToken(c)),
+            _ => return Err(BadToken(c)),
         }
     }
 
     if parens.len() > 0 {
-        return Err(CalcError::MismatchedParens);
+        return Err(MismatchedParens);
     }
 
     Ok(tokens)
@@ -102,6 +122,8 @@ fn parse<T: AsRef<str>>(expr: T) -> Result<Vec<Token>, CalcError> {
 
 /// Transforms infix tokens into postfix (RPN).
 fn postfix(mut tokens: Vec<Token>) -> Vec<Token> {
+    use Token::*;
+
     let mut queue: Vec<Token> = Vec::new();
     let mut stack: Vec<Token> = Vec::new();
 
@@ -109,16 +131,16 @@ fn postfix(mut tokens: Vec<Token>) -> Vec<Token> {
 
     while let Some(token) = tokens.pop() {
         match token {
-            Token::Operand(_) | Token::UnOp(_) => queue.push(token),
-            Token::BiOp(_) => {
+            Operand(_) | UnOperator(_) => queue.push(token),
+            BiOperator(_) => {
                 while !stack.is_empty() && stack[stack.len() - 1] >= token {
                     queue.push(stack.pop().unwrap());
                 }
                 stack.push(token);
             }
-            Token::Bracket('(') => stack.push(token),
-            Token::Bracket(')') => {
-                while !stack.is_empty() && stack[stack.len() - 1] != Token::Bracket('(') {
+            Bracket('(') => stack.push(token),
+            Bracket(')') => {
+                while !stack.is_empty() && stack[stack.len() - 1] != Bracket('(') {
                     queue.push(stack.pop().unwrap());
                 }
                 stack.pop();
@@ -136,43 +158,52 @@ fn postfix(mut tokens: Vec<Token>) -> Vec<Token> {
 
 /// Evaluates an RPL expression.
 fn evaluate(mut tokens: Vec<Token>) -> Result<f32, CalcError> {
+    use Token::*;
+    use BiOperation::*;
+    use UnOperation::*;
+    use CalcError::*;
+
     let mut stack: Vec<f32> = Vec::new();
-    let mut unary: Vec<UnOperator> = Vec::new();
+    let mut unary: Vec<UnOperation> = Vec::new();
 
     tokens.reverse();
 
     while let Some(token) = tokens.pop() {
         match token {
-            Token::UnOp(operator) => unary.push(operator),
-            Token::Operand(mut n) => {
+            UnOperator(operator) => unary.push(operator),
+            Operand(mut n) => {
                 while let Some(operator) = unary.pop() {
                     n = match operator {
-                        UnOperator::Plus => n,
-                        UnOperator::Minus => -n,
+                        Plus => n,
+                        Minus => -n,
                     };
                 }
                 stack.push(n as f32)
             }
-            Token::BiOp(operator) => {
+            BiOperator(operator) => {
                 let (right, left): (f32, f32) = match (stack.pop(), stack.pop()) {
                     (Some(n1), Some(n2)) => (n1, n2),
-                    _ => return Err(CalcError::MissingOperand),
+                    _ => return Err(MissingOperand),
                 };
-                match operator {
-                    BiOperator::Add => stack.push(left + right),
-                    BiOperator::Sub => stack.push(left - right),
-                    BiOperator::Mul => stack.push(left * right),
-                    BiOperator::Div => stack.push(left / right),
-                    BiOperator::Mod => stack.push(left % right),
-                    BiOperator::Pow => stack.push(left.powf(right)),
-                }
+                let result = match operator {
+                    Add => left + right,
+                    Sub => left - right,
+                    Mul => left * right,
+                    Div => left / right,
+                    Mod => left % right,
+                    Pow => left.powf(right),
+                    And => ((left as i32) & (right as i32)) as f32,
+                    Or => ((left as i32) | (right as i32)) as f32,
+                    Xor => ((left as i32) ^ (right as i32)) as f32,
+                };
+                stack.push(result)
             }
             _ => {}
         }
     }
 
     if stack.len() > 1 {
-        return Err(CalcError::UnusedTokens);
+        return Err(UnusedTokens);
     }
 
     Ok(stack.pop().unwrap())
